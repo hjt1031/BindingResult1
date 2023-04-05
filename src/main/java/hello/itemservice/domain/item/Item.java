320134351cfa9a732fa2dfd43a5fe3a369163bee
juntae ha <hjt1031@naver.com>
2023-04-05 21:46:28 +0900

    Bean_validation 적용

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -14,15 +14,15 @@ public class Item {
 //    @NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
     private Long id;
 
-//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    @NotBlank
     private String itemName;
 
-//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
 
-//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-//    @Max(value = 9999, groups = {SaveCheck.class})
+    @NotNull
+    @Max(value = 9999)
     private Integer quantity;
 
     public Item() {
